b90f15c9cde2bc47f29e09b4731072239fdb5b98
David Chapman <talk2dc@gmail.com>
2024-06-21 14:12:36 -0400

    CSS for images

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,6 @@
 import "$styles/index.css";
 import "$styles/syntax-highlighting.css";
+import "$styles/images.css";
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}";
