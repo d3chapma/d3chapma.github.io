21eed8511a82246519dcb0c1c0d78e2b0ebf2636
David Chapman <talk2dc@gmail.com>
2024-07-04 11:08:55 -0400

    Add SEO plugin

--- config/initializers.rb
+++ config/initializers.rb
@@ -48,6 +48,8 @@ Bridgetown.configure do |config|
   # init :"bridgetown-routes"
   #
 
+  init :"bridgetown-seo-tag"
+
   # We also recommend that if you're using Roda routes you include this plugin
   # so you can get a generated routes list in `.routes.json`. You can then run
   # `bin/bridgetown roda:routes` to print the routes. (This will require you to
